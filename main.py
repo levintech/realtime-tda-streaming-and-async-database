291ef41a6defc2a7212a0d0cdfea3e8696c2da3c
DraganKojic <dkojic325@gmail.com>
2021-07-30 01:53:42 +1000

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import psutil
+import sys
+import time
+from subprocess import Popen
+from subprocess import call
+
+while True:
+    is_running = False
+    for process in psutil.process_iter():
+        if process.cmdline() == ['python3', 'tda-streaming.py']:
+            print('script is running')
+            is_running = True
+            time.sleep(1)
+            break
+    
+    if not is_running:
+        call(["python3", "tda-streaming.py"])
+        print('run the script again')
